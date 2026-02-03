58ded2a1bf70cd59bb88fb61a03d3563f2dd67ef
he55 <40764272+he55@users.noreply.github.com>
2026-02-03 23:20:16 +0800

    Update DirectoryDto.java

--- file-service/src/main/java/com/example/dto/DirectoryDto.java
+++ file-service/src/main/java/com/example/dto/DirectoryDto.java
@@ -2,8 +2,11 @@ package com.example.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class DirectoryDto {
     private String name;
     private String path;
+    private List<DirectoryDto> children;
 }
