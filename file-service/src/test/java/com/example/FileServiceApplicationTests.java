489023faf13e9d81203e8db56d6473e98e2b5565
he55 <40764272+he55@users.noreply.github.com>
2026-02-05 22:46:41 +0800

    test

--- file-service/src/test/java/com/example/FileServiceApplicationTests.java
+++ file-service/src/test/java/com/example/FileServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.example.dto.DirectoryDto;
+import com.example.dto.FileDto;
 import com.example.service.FileService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,14 @@ class FileServiceApplicationTests {
 
     @Test
     void getDirectories() {
-        List<DirectoryDto> directories = fileService.getDirectories();
+        List<DirectoryDto> directories = fileService.getDirectories(645);
         System.out.println("directories = " + directories);
+    }
+
+    @Test
+    void getFiles() {
+        List<FileDto> files = fileService.getFiles("645/照片");
+        System.out.println("files = " + files);
     }
 
 }
