b60d61fc341d5e48e02cc6ac2e3d8f553bcf286a
he55 <40764272+he55@users.noreply.github.com>
2026-02-05 22:45:13 +0800

    use properties

---
+++ file-service/src/main/java/com/example/StorageProperties.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@ConfigurationProperties("storage")
+@Component
+public class StorageProperties {
+    private String location = "upload-dir";
+}
