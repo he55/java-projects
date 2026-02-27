56e4afd8acb1c3153a4fa2178bfdf7087409c9a1
he55 <40764272+he55@users.noreply.github.com>
2026-02-28 00:03:21 +0800

    add line

--- file-service/src/main/java/com/example/handler/JwtAccessDeniedHandler.java
+++ file-service/src/main/java/com/example/handler/JwtAccessDeniedHandler.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import java.io.IOException;
 
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
--- file-service/src/main/java/com/example/handler/JwtAuthenticationEntryPoint.java
+++ file-service/src/main/java/com/example/handler/JwtAuthenticationEntryPoint.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import java.io.IOException;
 
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
