0ef83bf39a474646b9316dfef298add410dd8b3a
he55 <40764272+he55@users.noreply.github.com>
2026-03-04 22:32:36 +0800

    Update AuthServiceImpl.java

--- file-service/src/main/java/com/example/service/AuthServiceImpl.java
+++ file-service/src/main/java/com/example/service/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
 import com.example.dto.UserDto;
 import com.example.dto.req.LoginDto;
 import com.example.dto.resp.UserTokenDto;
@@ -8,6 +9,9 @@ import com.example.pojo.User;
 import com.example.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.util.Set;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -18,7 +22,16 @@ public class AuthServiceImpl implements AuthService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private DataSource dataSource;
+
     public UserTokenDto login(LoginDto loginDto) {
+        DynamicRoutingDataSource source = (DynamicRoutingDataSource) dataSource;
+        Set<String> dsNames = source.getDataSources().keySet();
+        if (!dsNames.contains(loginDto.getOrg())) {
+            throw new BusinessException("机构编号无效");
+        }
+
         User user = userService.findByIdAndPassword(loginDto.getUserId(), loginDto.getPassword(), loginDto.getOrg());
         if (user == null) {
             throw new BusinessException("用户名或者密码错误");
