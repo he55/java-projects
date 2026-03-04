4477534e359d5395f7582eb7ce83650640221b37
he55 <40764272+he55@users.noreply.github.com>
2026-03-04 22:20:19 +0800

    Update UserService.java

--- file-service/src/main/java/com/example/service/UserService.java
+++ file-service/src/main/java/com/example/service/UserService.java
@@ -2,6 +2,9 @@ package com.example.service;
 
 import com.example.pojo.User;
 
+import java.util.List;
+
 public interface UserService {
     User findByIdAndPassword(int id, String password, String org);
+    List<User> findAll();
 }
