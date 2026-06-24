52c68519ceed139edef540c04fe587e9dc20384b
wei <40764272+he55@users.noreply.github.com>
2026-06-25 01:43:14 +0800

    Update ShortLinkController.java

--- common-service/src/main/java/com/example/commonservice/controller/ShortLinkController.java
+++ common-service/src/main/java/com/example/commonservice/controller/ShortLinkController.java
@@ -3,7 +3,6 @@ package com.example.commonservice.controller;
 import com.example.commonservice.dto.req.ShortLinkCreateReqDTO;
 import com.example.commonservice.dto.resp.ShortLinkCreateRespDTO;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -19,15 +17,17 @@ import java.time.Duration;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/short")
 public class ShortLinkController {
 
     static final String SHORT_LINK_KEY = "short_link:%s";
 
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
 
-    @PostMapping
+    public ShortLinkController(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @PostMapping("/short-link")
     public ShortLinkCreateRespDTO createShortLink(@Valid @RequestBody ShortLinkCreateReqDTO shortLinkCreateReqDTO) {
         String key = UUID.randomUUID().toString().replace("-", "");
 
@@ -35,12 +35,12 @@ public class ShortLinkController {
                 .set(SHORT_LINK_KEY.formatted(key), shortLinkCreateReqDTO.getUrl(), Duration.ofDays(1));
 
         ShortLinkCreateRespDTO shortLinkCreateRespDTO = new ShortLinkCreateRespDTO();
-        shortLinkCreateRespDTO.setShortUrl("http://localhost:8080/short/" + key);
+        shortLinkCreateRespDTO.setShortUrl("http://localhost:8080/u/" + key);
         shortLinkCreateRespDTO.setOriginUrl(shortLinkCreateReqDTO.getUrl());
         return shortLinkCreateRespDTO;
     }
 
-    @GetMapping("/{shortLink}")
+    @GetMapping("/u/{shortLink}")
     public ResponseEntity<?> resolveShortLink(@PathVariable String shortLink) {
         String url = stringRedisTemplate.opsForValue().get(SHORT_LINK_KEY.formatted(shortLink));
         if (url == null) {
